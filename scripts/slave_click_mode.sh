c8e6d5c95ef7a61de970f42196b95efa23cf1b17
jtan96 <jtman1226@gmail.com>
2019-01-23 12:05:14 -0800

    ran roscore and roslaunch in background

--- scripts/slave_click_mode.sh
+++ scripts/slave_click_mode.sh
@@ -4,7 +4,7 @@ echo "Click Mode: Slave"
 
 killall roscore
 killall roslaunch
-roscore
-roslaunch phantomx_rst arm.launch
-roslaunch rosbridge_server rosbridge_websocket.launch
+roscore &
+roslaunch phantomx_rst arm.launch &
+roslaunch rosbridge_server rosbridge_websocket.launch &
 roslaunch hit_based_robot_control hit_num_robot_control.launch
