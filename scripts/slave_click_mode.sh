e8b3cfe65e769b0bf2f291c3caa0e40200324e97
jtan96 <jtman1226@gmail.com>
2019-01-23 11:20:05 -0800

    cleaned up click mode script

---
+++ scripts/slave_click_mode.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo "Click Mode: Slave"
+
+killall roscore
+killall roslaunch
+roscore
+roslaunch phantomx_rst arm.launch
+roslaunch rosbridge_server rosbridge_websocket.launch
+roslaunch hit_based_robot_control hit_num_robot_control.launch
