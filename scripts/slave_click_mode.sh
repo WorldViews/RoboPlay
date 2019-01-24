63b83b85ab648864ba0373b5dcb6c9a5bdc3e351
jtan96 <jtman1226@gmail.com>
2019-01-23 16:39:41 -0800

    Increased sleep to 10

--- scripts/slave_click_mode.sh
+++ scripts/slave_click_mode.sh
@@ -6,5 +6,6 @@ killall roscore
 killall roslaunch
 roscore &
 roslaunch phantomx_rst arm.launch &
+sleep 10
 roslaunch rosbridge_server rosbridge_websocket.launch &
-roslaunch hit_based_robot_control hit_num_robot_control.launch
+roslaunch hit_based_robot_control hit_num_robot_control.launch &
