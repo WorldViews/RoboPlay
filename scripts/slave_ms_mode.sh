b4405db1d2d6c4ceb41e01f3e52e272855e5399b
jtan96 <jtman1226@gmail.com>
2019-01-28 19:42:36 -0800

    Changed while loop to Until loop

--- scripts/slave_ms_mode.sh
+++ scripts/slave_ms_mode.sh
@@ -5,14 +5,5 @@ echo "MS Mode: Slave"
 killall roscore
 roscore > ./roscore.log &
 sleep 5
-while [ 1 ] ; do
-        echo 'begin checking for roscore'
-        check_process "roscore" # the thread name
-        CHECK_RET = $?
-        if [ $CHECK_RET -eq 1 ]; # exists
-        then
-          roslaunch phantomx_rst arm.launch
-        fi
-        sleep 10
-done
-# roslaunch phantomx_rst arm.launch
+until rostopic list ; do sleep 1; done
+roslaunch phantomx_rst arm.launch
