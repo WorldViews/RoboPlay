248371316a3cbfed930721c37b36eaa54c5ad40f
jtan96 <jtman1226@gmail.com>
2019-01-28 19:25:02 -0800

    added while loop to check for running roscore

--- scripts/slave_ms_mode.sh
+++ scripts/slave_ms_mode.sh
@@ -4,5 +4,15 @@ echo "MS Mode: Slave"
 
 killall roscore
 roscore > ./roscore.log &
-#sleep 5
-roslaunch phantomx_rst arm.launch
+sleep 5
+while [ 1 ] ; do
+        echo 'begin checking for roscore'
+        check_process "roscore" # the thread name
+        CHECK_RET = $?
+        if [ $CHECK_RET -eq 1 ]; # exists
+        then
+          roslaunch phantomx_rst arm.launch
+        fi
+        sleep 10
+done
+# roslaunch phantomx_rst arm.launch
